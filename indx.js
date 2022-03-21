e16eab07c9575386357808b1bd6602b7eefa437e
ewfwefergvr <96682909+ewfwefergvr@users.noreply.github.com>
2022-03-21 22:25:59 +0200

    Create indx.js

---
+++ indx.js
@@ -0,0 +1,61 @@
+
+import { blacklist } from './blacklist.js';
+import expressIpFilter from 'express-ipfilter';
+const { IpDeniedError, IpFilter } = expressIpFilter;
+import expressPinoLogger from 'express-pino-logger';
+import { customAlphabet } from 'nanoid';
+const nanoid = customAlphabet('6789BCDFGHJKLMNPQRTWbcdfghjkmnpqrtwz', 16);
+export function setRequestID() {
+    return (req, res, next) => {
+        const id = req.headers['x-request-id'] || req.headers['cf-request-id'] || nanoid();
+        req['id'] = id;
+        res.set('X-Request-ID', id);
+        next();
+    };
+}
+export function logger(logger) {
+    return expressPinoLogger({
+        logger,
+        genReqId: (req) => {
+            return req.id;
+        },
+        reqCustomProps: (req, _res) => {
+            const body = req.body;
+            if (!body || req?.method !== 'POST' || req?.url !== '/')
+                return {};
+            return {
+                rpc: {
+                    method: body?.method,
+                    params: body?.params,
+                },
+            };
+        },
+    });
+}
+export function blacklistIPs() {
+    const ipLookup = () => {
+        return [...(blacklist('IPs') || [])];
+    };
+    return IpFilter(ipLookup, {
+        mode: 'deny',
+        log: false,
+        trustProxy: true,
+    });
+}
+export function handleErrors() {
+    return (err, req, res, next) => {
+        if (err instanceof IpDeniedError) {
+            // TODO: req.log.info(...);
+            res.status(403).end();
+        }
+        else {
+            next();
+        }
+    };
+}
+export default {
+    setRequestID,
+    logger,
+    blacklistIPs,
+    handleErrors,
+};
